c85a03316aa04c2770e076ee98a6388e218e5f91
khanontheweb <mkhanongm@gmail.com>
2020-02-12 13:20:42 -0500

    Method to get playlists from username

--- index.js
+++ index.js
@@ -37,7 +37,50 @@ request.post(authOptions, (error, response, body) => {
       console.log(error);
 });
 
-
+/**
+ * Get playlist IDs of the user denoted by paramater.
+ * 
+ * Goes through public playlists only. If a user has more than 50 playlists, then the 
+ * function will make another request to retrieve the other ids.
+ * 
+ * @param {String} id User id that is inputted into the url for the API call.
+ * 
+ * @return {Array} Returns an array of playlists id.
+ */
+async function getPlaylists(id) {
+    // Set up the options for the initial API call
+    let options = {
+      url: `https://api.spotify.com/v1/users/${id}/playlists`,
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      json: true,
+      qs: {
+        'limit': '20',
+      }
+    };
+  
+    try {
+      let response = await rp.get(options);
+    
+      let playlists = new Map();
+      response.items.forEach(item => {
+        playlists.set(item.id, new Playlist(item.name, item.external_urls.spotify,item.tracks.href,item.owner.id));
+      });
+      while(response.next) {
+        options.url = response.next; // We need to change the url end point to have the proper offset
+        response = await rp.get(options);
+        // Merge the arrays
+        response.items.forEach(item => {
+          playlists.set(item.id, new Playlist(item.name, item.external_urls.spotify,item.tracks.href,item.owner.id));
+        });
+      }
+      return playlists;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
 
 routes.route('/:id').get(async(req, res) => {
     try {
